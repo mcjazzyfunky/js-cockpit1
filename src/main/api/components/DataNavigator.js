bea175adb9ddaed8ad532aa1578bed62551c4dd3
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-06-05 10:06:40 +0200

    Some updates

--- src/main/api/components/DataNavigator.js
+++ src/main/api/components/DataNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { defineComponent } from 'js-widgets';
+
+import HBox from './HBox';
 import Paginator from './Paginator';
 
 export default defineComponent({
@@ -8,8 +10,19 @@ export default defineComponent({
   main: class extends React.Component {
     render() {
       return (
-        <div style={{ padding: '14px'}}>
-          <Paginator type="simple" pageIndex={24} pageSize={50} totalItemCount={1234} />
+        <div style={{ padding: '14px', width: '100%', height: '100%' }}>
+
+          <HBox>
+            <HBox.Cell key={3}>
+              AAA
+            </HBox.Cell>
+            <HBox.Cell>
+              BBB
+            </HBox.Cell>
+          </HBox>
+
+          { // <Paginator type="simple" pageIndex={24} pageSize={50} totalItemCount={1234} />
+          }
          </div>
       );
     }
